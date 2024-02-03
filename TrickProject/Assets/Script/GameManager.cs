6c37d18c317a6bc9941c563ad95631f5ed87cab5
DESKTOP-OPAB41O\user <molang0120@naver.com>
2024-02-04 07:39:51 +0900

    DoTween, UIEffect, UIGradient, AnimationSequencer, TextMeshAnimator, TextMesh Import Main, Play Scene 추가

---
+++ TrickProject/Assets/Script/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] Buttons;
+
+
+    private static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    enum MainButton { Start, End, }
+
+    public void OnClick_MainButton ()
+    {
+        if (Buttons[(int)MainButton.Start])
+        {
+            SceneManager.LoadScene("Play");
+        }
+    }
+}
